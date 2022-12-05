70ce70f6f8070f0489941615d0606fed4000b121
3v0k4 <riccardo.odone@gmail.com>
2022-12-05 12:13:04 +0100

    docs: point editUrl to main (not gh-pages)

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/KnapsackPro/docs.knapsackpro.com/tree/gh-pages/docusaurus/',
+          editUrl: 'https://github.com/KnapsackPro/docs.knapsackpro.com/tree/main/docusaurus/',
         },
         blog: false,
         theme: {
