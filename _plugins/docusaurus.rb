932d2eabecaebe44aac9b39096e5c878705712a1
3v0k4 <riccardo.odone@gmail.com>
2022-12-08 09:57:42 +0100

    docs(docusaurus): deploy jest and cypress docs

--- _plugins/docusaurus.rb
+++ _plugins/docusaurus.rb
@@ -10,4 +10,10 @@ Jekyll::Hooks.register :site, :post_write do |page|
   system!("cp -R docusaurus/build/img _site")
   system!("cp -R docusaurus/build/assets _site")
   system!("cp docusaurus/build/knapsack_pro-ruby/guide/index.html _site/knapsack_pro-ruby/guide")
+
+  system!("mkdir -p _site/cypress/guide")
+  system!("cp docusaurus/build/cypress/guide/index.html _site/cypress/guide")
+
+  system!("mkdir -p _site/jest/guide")
+  system!("cp docusaurus/build/jest/guide/index.html _site/jest/guide")
 end
