47fc8afffa36f410541d7606c4071e2eb2554be5
Artur Trzop <arturtrzop@gmail.com>
2025-01-16 22:25:17 +0100

    swizzling A component using wrapping to add nofollow to external links except to the main knapsackpro.com domain

---
+++ docusaurus/src/theme/MDXComponents/A.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import A from '@theme-original/MDXComponents/A';
+import type AType from '@theme/MDXComponents/A';
+import type {WrapperProps} from '@docusaurus/types';
+
+type Props = WrapperProps<typeof AType>;
+
+export default function AWrapper(props: Props): JSX.Element {
+  const { href } = props;
+  const isExternal =
+    href?.startsWith('http') &&
+    !href?.startsWith('https://knapsackpro.com') &&
+    !href?.includes(window.location.origin);
+
+  return (
+    <>
+      <A
+        {...props}
+        rel={isExternal ? 'nofollow noopener noreferrer' : undefined}
+        target={isExternal ? '_blank' : undefined}
+      />
+    </>
+  );
+}
